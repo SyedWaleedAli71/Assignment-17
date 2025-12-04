9260ef23b348d53f5a8897d72c46f9f9e8380f0d
SyedWaleedAli71 <syedwaleed887@gmail.com>
2025-12-04 09:11:15 +0500

    first_commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Assignment # 17 
 // STRING METHODS 
+
 // Q.1
 // let firstName = prompt("Enter your First Name..");
 // let lastName = prompt("Enter your Last Name..");
@@ -8,10 +9,53 @@
 
 // document.writeln("Hello.." + " " + fullName);
 // 
+
 // Q.2
 // let userInput = prompt("Enter your Favorite Mobile Phone Model..");
 // let length = userInput.length;  
 
 // document.writeln("My Favorite Phone is :" + " " +userInput);
 // 
+
+// Q.3
+// let text = "Pakistan";
+// console.log(text.slice(-1));
+// indexNo
+// let word = "Pakistan";
+// let indexNo = word.indexOf("n");
+
+// console.log(indexNo);
 // 
+
+// Q.4
+// let world = "Hello World";
+// console.log("Last Index Word is .." + world.slice(9,-1));
+// 
+// let worldNo = "Hello World";
+// let lastIndex = worldNo.lastIndexOf("l");
+// console.log("Last Index No is .." + lastIndex)
+// 
+
+// Q.5
+// let word = "Pakistani";
+// let indexWord = word.slice(3,-5);
+// let indexNo = word.indexOf("i");
+
+// console.log(indexWord + " : " + indexNo);
+// 
+// let word = "Pakistani";
+// let character = word.slice(3,-5);
+
+// console.log("Character at index 3 is: " + character);
+
+// 
+// Q.6
+// let firstName= prompt("Enter Your First Name..");
+// let lastName = prompt("Enter your Last Name..");
+
+// let fullName = firstName.concat(" ", lastName);
+
+// alert("Hello.." + fullName);
+// 
+
+//Q.7 
